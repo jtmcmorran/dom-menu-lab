089fd36345aa100227c93eafe51c9528d738963d
James McMorran <jamesmcmorran@Jamess-MBP.myfiosgateway.com>
2021-05-08 14:17:09 -0400

    Fixed typos

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-test
+//task 1.0
+const mainEl = document.querySelector('main');
+
+//task 1.1
+mainEl.style.backgroundColor = 'var(--main-bg)';
+
+//task1.2
+mainEl.innerHTML = '<h1>SEI Rocks!</h1>';
